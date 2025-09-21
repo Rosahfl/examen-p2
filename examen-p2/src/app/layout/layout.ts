93722ef8edd0ae791fdca346457545f40d238c15
unknown <hinojosac069@gmail.com>
2025-09-21 02:26:49 -0400

    feat(home): integrate bootstrap template into Home

---
+++ examen-p2/src/app/layout/layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../node_modules/@angular/router/router_module.d";
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class Layout {
+
+}
