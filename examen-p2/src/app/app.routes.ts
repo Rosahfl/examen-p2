d9a527cad96c1bc1ae3fef76e5b1b9e9419b2eb3
unknown <hinojosac069@gmail.com>
2025-09-21 03:54:54 -0400

    feat(router+a11y): routes and basic accessibility improvements

--- examen-p2/src/app/app.routes.ts
+++ examen-p2/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+        
+      {
+        path: '',
+        component: Home,
+        title: 'Inicio'
+      },
+      {
+        path: 'about',
+        component: About,
+        title: 'Acerca de'
+      },
+      {
+        path: 'contact',
+        component: Contact,
+        title: 'Contacto'
+      }
+    ]
+  },
+  // RUTA WILDCARD (importante en producción)
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
