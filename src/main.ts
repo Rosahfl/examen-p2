1cc0db3a649b838110516efb964e112f32e15e12
unknown <hinojosac069@gmail.com>
2025-09-23 10:22:24 -0400

    actualizando

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideServiceWorker } from '@angular/service-worker';
-import { isDevMode } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(App, {
   providers: [
-    appConfig.providers,
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ]
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
+  ],
 });
 
 
